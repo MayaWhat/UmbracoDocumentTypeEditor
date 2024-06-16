2fcd647823b64a5cf5432f27cc0efaed5836a2e8
MayaWhat <ian@devolutious.co.nz>
2024-06-16 14:58:36 +1200

    Some little tidy ups

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { UmbracoFS } from './umbracoFs';
-import { UmbracoApi } from './umbracoApi';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "umbracodocumenttypeeditor" is now active!');
+	// TODO: there's probably a better way to handle this, but it's pretty common for localhost
+	// to not have valid certs
 	process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
 	const umbracoFS = new UmbracoFS();
